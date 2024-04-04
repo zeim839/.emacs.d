e10f0f5d3e86b8101ff153090cedf4af60295c4a
zeim839 <zeim839@users.noreply.github.com>
2024-04-04 19:29:17 -0400

    set linux theme to ef-duo-dark

--- features/fe-linux.el
+++ features/fe-linux.el
@@ -4,4 +4,10 @@
 (use-package emacs
   :if (eq system-type 'gnu/linux)
   :config
-  (set-face-attribute 'default nil :font "JetBrains Mono" :height 115))
+  (set-face-attribute 'default nil :font "JetBrains Mono" :height 115)
+  (use-package ef-themes :ensure t
+    :init (load-theme 'ef-duo-dark :no-confirm)))
+
+(use-package tree-sitter-langs :ensure t :defer t)
+
+(setq epg-gpg-program "/usr/bin/gpg")
