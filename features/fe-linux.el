b98bf7d57628e61902d654506a60d57b1d07532f
zeim839 <zeim839@users.noreply.github.com>
2024-06-09 10:58:26 -0400

    features/fe-linux: specify correct gpg directory

--- features/fe-linux.el
+++ features/fe-linux.el
@@ -5,9 +5,8 @@
   :if (eq system-type 'gnu/linux)
   :config
   (set-face-attribute 'default nil :font "JetBrains Mono" :height 115)
+  (setq epg-gpg-program "/usr/bin/gpg")
   (use-package ef-themes :ensure t
     :init (load-theme 'ef-duo-dark :no-confirm)))
 
 (use-package tree-sitter-langs :ensure t :defer t)
-
-(setq epg-gpg-program "/usr/bin/gpg")
