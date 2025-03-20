0ab5b001be9662a218b7170f59f8df3b3d685af7
zeim839 <zeim839@users.noreply.github.com>
2025-03-20 14:41:57 -0400

    fix pinentry

--- features/fe-org.el
+++ features/fe-org.el
@@ -24,14 +24,17 @@
                  :matchers ("begin" "$1" "$" "$$" "\\(" "\\[")))
 
   :bind ("C-c a" . org-agenda)
-  :hook (org-mode . turn-on-flyspell)
+  :hook
+  (org-mode . turn-on-flyspell)
+  (org-mode . visual-line-mode)
   :config
   ;; Org-mode Encryption
   (use-package epa-file
-    :config
-    (fset 'epg-wait-for-status 'ignore)
-    (custom-set-variables '(epg-gpg-program  "/usr/local/bin/gpg"))
-    :init (epa-file-enable))
+    :init (epa-file-enable)
+    :config (fset 'epg-wait-for-status 'ignore)
+    :custom
+    (epg-pinentry-mode 'loopback)
+    (epg-gpg-program  "/usr/local/bin/gpg"))
 
   (font-lock-add-keywords
    'org-mode
@@ -56,7 +59,8 @@
    'org-babel-load-languages
    '((emacs-lisp . t)
      (python . t)
-     (jupyter . t)))
+     (jupyter . t)
+     (haskell . t)))
   :custom
   (org-babel-python-command "python3"))
 
@@ -79,7 +83,7 @@
   ("C-c n l" . org-roam-buffer-toggle)
   ("C-c n c" . org-roam-capture)
   ("C-c n d" . org-id-get-create)
-  ("C-c n a" . org-roam-alias-add)
+  ("C-c n a" . org-roam-dailies-capture-today)
   ("C-c n r" . org-roam-ref-add)
   ("C-c n f" . org-roam-node-find)
   ("C-c n i" . org-roam-node-insert)
@@ -96,9 +100,47 @@
   :ensure t :defer t
   :bind ("C-c t" . gts-do-translate)
   :custom
-  (gts-translate-list '(("en" "el") ("el" "en")))
+  (gts-translate-list '(("en" "el") ("el" "en") ("en" "ru") ("ru" "en")))
   (gts-default-translator
    (gts-translator
     :picker (gts-prompt-picker)
     :engines (list (gts-google-engine) (gts-google-rpc-engine))
     :render (gts-buffer-render))))
+
+(use-package elfeed
+  :ensure t :defer t
+  :bind ("C-c w e" . elfeed)
+  :custom
+  (elfeed-db-directory "~/.emacs.d/.cache/elfeed")
+  (elfeed-feeds '("https://planet.emacslife.com/atom.xml"
+                  "https://oneofus.la/have-emacs-will-hack/feed.xml"
+                  "http://oremacs.com/atom.xml"
+                  "https://protesilaos.com/master.xml"
+                  "https://vitalik.eth.limo/feed.xml"
+                  "http://bair.berkeley.edu/blog/feed.xml"
+                  "https://karpathy.github.io/feed.xml"
+                  ;;"https://rss.arxiv.org/rss/econ+q-fin"
+                  ;;"https://rss.arxiv.org/rss/cs.AI+cs.CV+cs.LG"
+                  ;;"https://rss.arxiv.org/rss/cs.DS+cs.DC"
+                  "https://www.philenews.com/feed/"
+                  "https://feeds.bbci.co.uk/news/world/rss.xml"
+                  "https://feeds.bbci.co.uk/news/world/europe/rss.xml"
+                  "https://feeds.bbci.co.uk/news/world/middle_east/rss.xml"
+                  "https://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml"
+                  "https://feeds.bbci.co.uk/news/business/rss.xml"
+                  "https://feeds.bbci.co.uk/news/technology/rss.xml"
+                  "https://geohot.github.io/blog/feed.xml"
+                  "https://feeds.a.dj.com/rss/RSSWSJD.xml"
+                  "https://feeds.a.dj.com/rss/RSSMarketsMain.xml"
+                  "https://feeds.a.dj.com/rss/WSJcomUSBusiness.xml"
+                  "https://feeds.a.dj.com/rss/RSSWorldNews.xml"
+                  "https://feeds.a.dj.com/rss/RSSOpinion.xml"
+                  "https://developer.apple.com/news/rss/news.rss"
+                  "https://www.jmlr.org/jmlr.xml")))
+
+;; Generates citations in PDF exports. This is supposedly
+;; deprecated, but it was the simplest solution for resolving
+;; the citeproc dependency on MacOS.
+(use-package citeproc-org
+  :ensure t :defer t
+  :init (citeproc-org-setup))
